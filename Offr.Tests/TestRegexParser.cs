8b81f08aa71b5dd8ae755283b1ae43aad0b4e246
Miles Thompson <utunga@gmail.com>
2009-05-24 22:37:46 +1200

    added a much simpler test

--- Offr.Tests/TestRegexParser.cs
+++ Offr.Tests/TestRegexParser.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using Offr.Message;
+using Offr.Text;
 
 namespace Offr.Tests
 {
@@ -18,7 +19,7 @@ namespace Offr.Tests
         }
 
         [Test]
-        public void TestRegexMethod()
+        public void TestRegexMethodCleverTest()
         {
             //NOTETOFIN: you might want to screw with the MockData.RawMessages aray to get a more friendly test set..
             MockMessageParser mockParser = new MockMessageParser();
@@ -34,5 +35,32 @@ namespace Offr.Tests
 
             }
         }
+
+        [Test]
+        public void TestRegexMethodSimpleTest()
+        {
+          
+            MockRawMessage raw = new MockRawMessage(0)
+            {
+                Timestamp = "10pm",
+                CreatedBy = MockData.User0,
+                Location = MockData.Location0,
+                MoreInfoURL = "http://bit.ly/message0Info",
+                OfferText = "#offr_test mulch available now in L:Paekakariki for #free #barter http://bit.ly/message0Info #mulch",
+                EndByText = null,
+                EndBy = null
+            };
+            raw.Tags.Add(new Tag(TagType.tag, "free"));
+            raw.Tags.Add(new Tag(TagType.tag, "barter"));
+            raw.Tags.Add(new Tag(TagType.tag, "mulch"));
+           
+
+            IOfferMessage message= (OfferMessage)_target.Parse(raw);
+            // actually this will certainly fail even for any kinda regex parser i can think of doing in a short time
+            Assert.AreEqual(3, message.Tags.Count, "Expect count of tags to be 3 for message " + raw);
+            Assert.AreEqual("#offr_test mulch available now in L:Paekakariki for #free #barter http://bit.ly/message0Info", message.OfferText, "Expect extracted message to work for " + raw);
+        
+        }
+
     }
 }
