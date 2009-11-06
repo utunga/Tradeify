f2459acd68963792d713a6bfd946a9cadf98e674
Miles Thompson <utunga@gmail.com>
2009-11-07 00:30:38 +1300

    Woops this is where I fixed up the test to give better info

--- Offr.Tests/TestGoogleLocation.cs
+++ Offr.Tests/TestGoogleLocation.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Web.Script.Serialization;
 using Offr.Location;
-using System.IO;
 using Offr.Text;
 using NUnit.Framework;
 
@@ -13,10 +9,9 @@ namespace Offr.Tests
     [TestFixture]
     public class TestGoogleLocation
     {
-
-        #region "1600+Amphitheatre+Parkway,+Mountain+View,+CA&output=json&oe=utf8"
-        //private string search =
-          // "1600+Amphitheatre+Parkway,+Mountain+View,+CA&output=json&oe=utf8";
+      
+        #region test json data
+        
         const string _testJSON = @"{
   ""name"": ""1500 Amphitheatre Parkway, Mountain View, CA"",
   ""Status"": {
@@ -63,69 +58,59 @@ namespace Offr.Tests
 }";
 
         #endregion
-        
+
         private SortedList<string, ILocation> _addressToExpectedTags;
 
         [TestFixtureSetUp]
-        public void Setup()
+        public void SetupTestData()
         {
             _addressToExpectedTags = new SortedList<string, ILocation>();
-            //_addressToExpectedTags.Add("30 Fitzroy Street, New Plymouth, NZ", new[] { "nz", "taranaki", "fitzroy"});
-            string address = "1600+Amphitheatre+Parkway,+Mountain+View,+CA&output=json&oe=utf8";
-            List<ITag> tags = new List<ITag>
+
+            string address = "1600 Amphitheatre Parkway, Mountain View, CA";
+            ILocation google = new Location.Location
+            {
+               GeoLat = (decimal) 37.4217590,
+               GeoLong = (decimal) -122.0843700,
+               Address = address,
+               LocationTags = new List<ITag>
                                   {
                                       (new Tag(TagType.loc, "Mountain View")),
                                       (new Tag(TagType.loc, "CA")),
                                       (new Tag(TagType.loc, "USA")),
                                       (new Tag(TagType.loc, "US"))
-                                  };
-            Console.WriteLine("tag count"+tags.Count);
-            Location.Location google = new Location.Location
-                                           {
-                                               GeoLat = (decimal) 37.4217590,
-                                               GeoLong = (decimal) -122.0843700,
-                                               SourceText = address,
-                                               _locationTags = tags
-
-                                           };
-            google._locationTags = tags;
+                                  }
+            };
             _addressToExpectedTags.Add(address, google);
-            address = "30+Fitzroy+Street,+New+Plymouth,+NZ&output=json&oe=utf";
-            tags = new List<ITag>
+
+            address = "30 Fitzroy Street, New Plymouth";
+            ILocation fitzroy = new Location.Location
+            {
+                GeoLat = (decimal)-39.0443597,
+                GeoLong = (decimal)174.1080569,
+                Address = address,
+                LocationTags = new List<ITag>
                                   {
                                       (new Tag(TagType.loc, "New Plymouth")),
                                       (new Tag(TagType.loc, "Taranaki")),
                                       (new Tag(TagType.loc, "New Zealand")),
                                       (new Tag(TagType.loc, "NZ"))
-                                  };
-            Location.Location fitzroy = new Location.Location
-            {
-                GeoLat = (decimal)-39.0443597,
-                GeoLong = (decimal)174.1080569,
-                SourceText = address,
-                _locationTags = tags
-
+                                  }
             };
-           fitzroy._locationTags = tags;
-            //_addressToExpectedTags.Add(address, fitzroy);
+            _addressToExpectedTags.Add(address, fitzroy);
 
-           address = "20+Lambton+Quay,+Wellington,+Wellington&output=json&oe=utf8";
-           tags = new List<ITag>
-                                  {
-                                      (new Tag(TagType.loc, "Wellington")),
-                                      (new Tag(TagType.loc, "Wellington")),
-                                      (new Tag(TagType.loc, "New Zealand")),
-                                      (new Tag(TagType.loc, "NZ"))
-                                  };
-           Location.Location lambton = new Location.Location
+           address = "20 Lambton Quay";
+           ILocation lambton = new Location.Location
            {
                GeoLat = (decimal)-41.2787026,
                GeoLong = (decimal)174.7785408,
-               SourceText = address,
-               _locationTags = tags
-
+               Address = address,
+               LocationTags = new List<ITag>
+                                  {
+                                      (new Tag(TagType.loc, "Wellington")),
+                                      (new Tag(TagType.loc, "New Zealand")),
+                                      (new Tag(TagType.loc, "NZ"))
+                                  }
            };
-           lambton._locationTags = tags;
            _addressToExpectedTags.Add(address, lambton);
         }
 
@@ -138,35 +123,46 @@ namespace Offr.Tests
         [Test]
         public void TestDeserialize()
         {
-            //StreamReader streamReader = new StreamReader("C:/Users/joav/Desktop/offr/Offr.Tests/GoogleLocationTest.txt");
-            //string text = streamReader.ReadToEnd();
-            //Console.WriteLine(text);
-            //streamReader.Close();
-           //Console.WriteLine(_testJSON);
             GoogleResultSet resultSet = (new JavaScriptSerializer()).Deserialize<GoogleResultSet>(_testJSON);
-            Assert.AreEqual("1500 Amphitheatre Parkway, Mountain View, CA", resultSet.name, "name did not serialize correctly");
-            
-            
-           
+            Assert.AreEqual("1500 Amphitheatre Parkway, Mountain View, CA", resultSet.name, "name did not serialize correctly");           
         }
-        [Test]
-        public void TestParse()
-        {
-            GoogleLocationProvider g=new GoogleLocationProvider();
-            foreach(string search in _addressToExpectedTags.Keys) {
-                ILocation location = g.Parse(search);
-                ILocation expected = _addressToExpectedTags[search];
-                List<ITag> list = location._locationTags;
-                List<ITag> tags = expected._locationTags;
-                //Console.WriteLine("count " + location._locationTags.Count);
-                Assert.IsTrue(location.Equals(expected), "Location not as expected");
-}
-            
-         // ILocation  location = g.Parse(search);
-           // Assert.AreEqual("USA", location.Country,"Country is not as expected");
-          //Assert.AreEqual(search, location.SourceText, "Country is not as expected");
-          //Assert.AreEqual(search, location.SourceText, "Country is not as expected");
 
+        [Test]
+        public void TestLiveGoogleParse()
+        {
+            // NOTE2J - renamed 'g' to 'locationProvider' don't ever, ever, *ever* call a variable a single letter - except 'i' and 'j' for loop counters, maybe
+            GoogleLocationProvider locationProvider = new GoogleLocationProvider();
+            foreach (string address in _addressToExpectedTags.Keys)
+            {
+                ILocation location = locationProvider.Parse(address);
+                ILocation expected = _addressToExpectedTags[address];
+                AssertLocationEquality(address, expected, location);
+            }
+
+        }
+
+        
+        private static void AssertLocationEquality(string forAddress, ILocation expected, ILocation actual)
+        {
+            //NOTE2J - do it this way you get much more useful debug data
+            //         and also save your self the trouble of having to override equals() etc etc..
+
+            Assert.AreEqual(expected.GeoLat, actual.GeoLat, "GeoLat for '" + forAddress + "' was not as expected");
+            Assert.AreEqual(expected.GeoLong, actual.GeoLat, "GeoLong for '" + forAddress + "' was not as expected");
+            Assert.AreEqual(expected.Address, actual.Address, "Address for '" + forAddress + "' was not as expected");
+
+            foreach (ITag locationTag in expected.LocationTags)
+            {
+                Assert.That(actual.LocationTags.Contains(locationTag), "Expected tag " + locationTag + " was not contained in result for " + forAddress);    
+            }
+
+            foreach (ITag locationTag in actual.LocationTags)
+            {
+                Assert.That(expected.LocationTags.Contains(locationTag), locationTag + " unexpectedly contained in result for " + forAddress);
+            }
+
+            // this would never happen.. but fwiw
+            Assert.AreEqual(expected.LocationTags.Count, actual.LocationTags.Count, "Somehow, inexplicably, the counts for expected vs actual location tags are different even though they contain the exact same set of tags" );
         }
 
         //[TestMethod]
