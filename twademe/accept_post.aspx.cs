c0078bd3a4f2880544e5792d77da1af0554b7a43
Joav Beerman <joav.beerman@gmail.com>
2009-12-04 12:50:26 +1300

    Added persistence to post calls

--- twademe/accept_post.aspx.cs
+++ twademe/accept_post.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,12 +11,20 @@ namespace twademe
 {
     public partial class accept_post : System.Web.UI.Page
     {
+        private static List<String> posts = new List<String>();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Request.Form["RawMessage"]!=null)
             {
-                Response.Write(Request.Form["RawMessage"].ToString());
+                string message = Request.Form["RawMessage"].ToString();
+                posts.Add(message);
+                foreach (string m in posts)
+                {
+                    Response.Write(m+" | ");     
+                }
+                
             }
         }
+
     }
 }
