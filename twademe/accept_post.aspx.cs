b14b9b874154fe34e41ba963fe692f91f045e334
Joav Beerman <joav.beerman@gmail.com>
2009-12-04 14:07:06 +1300

    post debugging

--- twademe/accept_post.aspx.cs
+++ twademe/accept_post.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -11,20 +12,31 @@ namespace twademe
 {
     public partial class accept_post : System.Web.UI.Page
     {
-        private static List<String> posts = new List<String>();
+        private static List<MessageWrapper> posts = new List<MessageWrapper>();
+        //private static List<String> rawMessage = new List<String>();
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Request.Form["RawMessage"]!=null)
+            MessageWrapper wrapper= new MessageWrapper();
+            NameValueCollection collection = Request.QueryString;
+            NameValueCollection form = Request.Form;
+            wrapper.raw = "";//= collection.ToString();
+            foreach (string key in form)
+                wrapper.raw += key + ",";
+            if (form["RawMessage"]!=null)
             {
-                string message = Request.Form["RawMessage"].ToString();
-                posts.Add(message);
-                foreach (string m in posts)
-                {
-                    Response.Write(m+" | ");     
-                }
+                wrapper.message = form["RawMessage"].ToString();
                 
             }
+            posts.Add(wrapper);
+            foreach (MessageWrapper w in posts)
+            {
+                Response.Write(" | " + w.raw + " : " + w.message + " | ");
+            }
         }
-
+        private class MessageWrapper
+    {
+            public string raw{ get; set;}
+            public string message { get; set; }
+    }
     }
 }
