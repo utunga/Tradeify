24d8764e818f9c8c1f4b84189ef4f5173b7305a7
utunga <utunga@gmail.com>
2010-01-04 12:44:52 +1300

    add opensocial_wrapper / mock_wrapper.js - which factor out the stuff specific to the ning/localhost containers

---
+++ twademe/osocial/mock_wrapper.js
@@ -0,0 +1,41 @@
+﻿/*
+  Javascript wrapper class that provides a mock version of relevant opensocial bits and pieces
+  for local testing purposes - include this *after* or instead of other wrappers for testing purposes
+*/
+
+//override this url
+    
+function mock_container() {
+
+    this.offers_uri = "/offers_json.aspx";
+    this.tags_uri = "/tags_json.aspx";
+    
+    //assumes test is running in local web
+    this.accept_post_url = "/accept_post.aspx";
+   
+    this.adjustHeight = function(height) {    
+        //does nothing in test
+    }
+    
+    this.postMessage = function(message) {
+       
+        var name = "utunga";
+        var thumbnail= "http://api.ning.com/files/e8j55wYy8dOF4G2Tc2xlF2SnUpF9HbyUgbsCA43rqCZ5eNG2TFQu5zb7JfXErTROYhPch5PaDGwsxuXMwwZSXSQtPap9zCDd/267662688.bin?crop=1%3A1";
+        var profileUrl ="http://utunga.ning.com/profile/MilesThompson";
+
+        var message_data = {
+            message:message,
+            username:name,
+            thumbnail:thumbnail,
+            namespace:"ooooby", //FIXME
+            profileurl:profileUrl
+        };
+        
+        
+        $.post(this.accept_post_url, message_data, function(data) {
+            alert("post:" + data);
+        });
+    }
+}
+
+var container = new mock_container();
---
+++ twademe/osocial/opensocial_wrapper.js
@@ -0,0 +1,62 @@
+﻿
+/*
+  Javascript wrapper class that wraps around all the open social/ning specific stuff (so that
+  it can be swapped out for other classes more easily / swapped out for local testing etc 
+*/
+    
+function OSocialContainer() {
+
+    this.offers_uri = "http://tradeify.org/offers_json.aspx";
+    this.tags_uri = "http://tradeify.org/tags_json.aspx";
+
+    this.accept_post_url = "http://tradeify.org/accept_post.aspx";
+    
+    this.adjustHeight = function(height) {    
+        gadgets.window.adjustHeight(height);
+    }
+        
+    /* ------------------------------
+         sending data from form
+       ------------------------------*/
+
+    this.postMessage = function(message) {
+        var message = message;
+        var req = opensocial.newDataRequest();
+        req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER), 'viewer');
+        req.send(function(response) {
+           
+            var viewer = response.get('viewer').getData();
+            var viewerJson= gadgets.json.stringify(viewer);
+            var name = viewer.getDisplayName();
+            var thumbnail= viewer.getField(opensocial.Person.Field.THUMBNAIL_URL,null);
+            var profileUrl = viewer.getField(opensocial.Person.Field.PROFILE_URL,null);
+
+            //not sure if we need/want this approach or not
+            var dataPacket = {};
+            dataPacket.Message = message;
+            dataPacket.User = viewer;
+            var dataPacketJSON = gadgets.json.stringify(dataPacket);
+            
+            var message_data = {
+                message:message,
+                username:name,
+                thumbnail:thumbnail,
+                namespace:"ooooby", //FIXME
+                profileurl:profileUrl,
+                DataPacket:dataPacketJSON
+            };
+            this.postToURL(this.accept_post_url,message_data);
+        });
+    }
+
+    this.postToURL = function(url, postdata) {
+        var params = {};
+        postdata = gadgets.io.encodeValues(postdata);
+        params[gadgets.io.RequestParameters.METHOD] = gadgets.io.MethodType.POST;
+        params[gadgets.io.RequestParameters.POST_DATA]= postdata;
+        var response=-1;
+        gadgets.io.makeRequest(url,response , params);
+    }
+}
+
+var container = new OSocialContainer();
