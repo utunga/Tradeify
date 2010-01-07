7d45733e980bf52d35c0add7ed9be00e5e56afaf
Joav Beerman <joav.beerman@gmail.com>
2010-01-07 16:21:38 +1300

    Added a console so we can test out the date time serialization

---
+++ OffrConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Offr.Json;
+
+namespace OffrConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+            Debug.Assert(time.Equals(JSON.Deserialize<DateTime>(JSON.Serialize(time))));
+            Console.Out.WriteLine("time :" + time.ToLongTimeString() + " JSON.Deserialize<DateTime>(JSON.Serialize(time)):" + JSON.Deserialize<DateTime>(JSON.Serialize(time)).ToLongTimeString());
+        }
+    }
+}
