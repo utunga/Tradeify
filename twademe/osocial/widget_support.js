b0561e4fcfca87da063b7ca30252af6cddedf007
Joav Beerman <joav.beerman@gmail.com>
2010-01-18 13:23:06 +1300

    clear markers fix

--- twademe/osocial/widget_support.js
+++ twademe/osocial/widget_support.js
@@ -24,12 +24,28 @@ function TradeifyWidget(offers_selector, current_tags_selector) {
 392 }); */
 
     }
+    google.maps.Map.prototype.markers = new Array();
+
+    google.maps.Map.prototype.getMarkers = function() {
+        return this.markers
+    };
+
     google.maps.Map.prototype.clearMarkers = function() {
         for (var i = 0; i < this.markers.length; i++) {
             this.markers[i].setMap(null);
         }
-        this.markers = new Array(); 
+        this.markers = new Array();
     };
+
+    google.maps.Marker.prototype._setMap = google.maps.Marker.prototype.setMap;
+
+    google.maps.Marker.prototype.setMap = function(map) {
+        if (map) {
+            map.markers[map.markers.length] = this;
+        }
+        this._setMap(map);
+    }
+
     function updateMap() {
         map.clearMarkers();
         $.each(offers, function() {
