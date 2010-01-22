3902ca0a4967e5a39c73ebd1d3a39fc3519b25ac
Joav Beerman <joav.beerman@gmail.com>
2010-01-22 16:16:20 +1300

    Added a dummy ignore user to test ignore user repository live 2

---
+++ OffrLib/Repository/IgnoredUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Offr.Demo;
+using Offr.Text;
+
+namespace Offr.Repository
+{
+    public class IgnoredUserRepository : BaseRepository<IUserPointer>, IIgnoredUserRepository, IPersistedRepository
+    {
+        public void InitializeFromFile()
+        {
+            /*
+            Save(new OpenSocialUserPointer("ooooby", "just_a_test",
+             "http://s3.amazonaws.com/twitter_production/profile_images/228862942/YinD_ContactSheet-003_normal.jpg",
+             ""));
+             */
+            Save(new OpenSocialUserPointer("ooooby", "Joav","http://s3.amazonaws.com/twitter_production/profile_images/228862942/YinD_ContactSheet-003_normal.jpg",""));
+        }
+
+    }
+}
