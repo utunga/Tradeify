db39f3884dc0a73eafa8004ba5a1ac6c8df02eb5
Joav Beerman <joav.beerman@gmail.com>
2010-01-29 22:26:21 +1300

    Submitted new message files

---
+++ OffrLib/Message/WantedMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Offr.Message
+{
+    public class WantedMessage:BaseMarketMessage
+    {
+        public static string HASHTAG = "#" + MessageType.wanted;
+        public override MessageType MessageType
+        {
+            get { return MessageType.wanted; }
+        }
+    }
+}
