d5c7f8e52254da2f13ca38717c1194ada1c4b530
Joav Beerman <joav.beerman@gmail.com>
2010-02-19 17:00:13 +1300

    tidied up pager code further

--- twademe/js/quickpager.jquery.js
+++ twademe/js/quickpager.jquery.js
@@ -6,18 +6,10 @@
 //		v1.1
 //		18/09/09 * bug fix by John V - http://blog.geekyjohn.com/
 //-------------------------------------------------
-//var pageCount=-2;
-//var selector;
-//var options;
-//var pageSelector;
-function initialPageClick(currentPage, pager) {
-    $(pager+".pagerPage").hide();
-    $(pager+".simplePagerPage" + currentPage).show();
-}
 
 (function($) {
 
-    $.fn.quickPager = function(options, pager,selector) {
+    $.fn.quickPager = function(options, pager, selector) {
 
         var defaults = {
             pageSize: 10,
@@ -25,19 +17,13 @@ function initialPageClick(currentPage, pager) {
             holder: null,
             pagerLocation: "after"
         };
-        var pageCount = -2;
-        var pageSelector = pager;
+        var pageCount;
         var options = $.extend(defaults, options);
-
-        //pageCount = (((this)[0]).firstChild.length / options.pageSize).toFixed(0) + 1;
         return this.each(function() {
 
 
             var selector = $(this);
             var pageCounter = 1;
-
-            //selector.wrap("<div class='simplePagerContainer'></div>");
-
             selector.children().each(function(i) {
 
                 if (i < pageCounter * options.pageSize && i >= (pageCounter - 1) * options.pageSize) {
@@ -51,81 +37,16 @@ function initialPageClick(currentPage, pager) {
                 }
 
             });
-            if (pageCount == -2) pageCount = pageCounter;
-            initialPageClick(options.currentPage);
-
-
-            if (pageCount < 1) {
+            if (pageCounter < 1) {
                 return;
             }
-            var pageClick= function (currentPage) {
+            var pageClick = function(currentPage) {
                 $(selector).children(".pagerPage").hide();
                 $(selector).children(".simplePagerPage" + currentPage).show();
-                $(pageSelector).pager({ pagenumber: currentPage, pagecount: pageCount, buttonClickCallback: pageClick });
+                $(pager).pager({ pagenumber: currentPage, pagecount: pageCounter, buttonClickCallback: pageClick });
             }
-            //selector.after("<div id=\"pager\" ></div>");
-            $(selector).children(".pagerPage").hide();
-            $(selector).children(".simplePagerPage" + 1).show();
-            $(pageSelector).pager({ pagenumber: 1, pagecount: pageCount, buttonClickCallback: pageClick });
+            pageClick(1);
         });
-
-
-        /*
-        //Build pager navigation
-        var pageNav = "<ul class='simplePagerNav'>";	
-        for (i=1;i<=pageCounter;i++){
-        if (i==options.currentPage) {
-        pageNav += "<li class='currentPage simplePageNav"+i+"'><a rel='"+i+"' href='#'>"+i+"</a></li>";	
-        }
-        else {
-        pageNav += "<li class='simplePageNav"+i+"'><a rel='"+i+"' href='#'>"+i+"</a></li>";
-        }
-        }
-        pageNav += "</ul>";
-			
-			if(!options.holder) {
-        switch(options.pagerLocation)
-        {
-        case "before":
-        selector.before(pageNav);
-        break;
-        case "both":
-        selector.before(pageNav);
-        selector.after(pageNav);
-        break;
-        default:
-        selector.after(pageNav);
-        }
-        }
-        else {
-        $(options.holder).append(pageNav);
-        }
-			
-			//pager navigation behaviour
-        selector.parent().find(".simplePagerNav a").click(function() {
-					
-				//grab the REL attribute 
-        var clickedLink = $(this).attr("rel");
-        options.currentPage = clickedLink;
-				
-				if(options.holder) {
-        $(this).parent("li").parent("ul").parent(options.holder).find("li.currentPage").removeClass("currentPage");
-        $(this).parent("li").parent("ul").parent(options.holder).find("a[rel='"+clickedLink+"']").parent("li").addClass("currentPage");
-        }
-        else {
-        //remove current current (!) page
-        $(this).parent("li").parent("ul").parent(".simplePagerContainer").find("li.currentPage").removeClass("currentPage");
-        //Add current page highlighting
-        $(this).parent("li").parent("ul").parent(".simplePagerContainer").find("a[rel='"+clickedLink+"']").parent("li").addClass("currentPage");
-        }
-				
-				//hide and show relevant links
-        selector.children().hide();			
-        selector.find(".simplePagerPage"+clickedLink).show();
-				
-				return false;
-        });
-        */
 
     }
 
